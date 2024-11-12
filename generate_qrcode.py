fa0d27ef3ef12fea28f45276dad808e12053aab8
Milica Stanojic <micist04@gmail.com>
2024-11-12 12:15:33 +0100

    Update generate_qrcode.py

--- generate_qrcode.py
+++ generate_qrcode.py
@@ -1,8 +1,13 @@
+import os
 import streamlit as st
 import segno
 import time
 
 def generate_qrcode():
+    # if directory does not exist
+    directory_path = 'images
+    os.makedirs(directory_path,exist_ok=True)
+    
     st.header("Generate QR Code")
     url = st.text_input("Enter the URL you want to encode:")
 
